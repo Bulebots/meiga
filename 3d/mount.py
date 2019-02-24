0288167db82a5f4b8b82197229570d813a4f55cc
claraC <clara.casas.castedo@gmail.com>
2019-02-24 21:13:05 +0100

    Include 3d mount design

---
+++ 3d/mount.py
@@ -0,0 +1,76 @@
+"""
+This script defines the mount for the wheels and motor.
+"""
+from math import asin
+from math import cos
+from math import sin
+from math import radians
+
+import cadquery
+from Helpers import show
+
+
+def circle_points(number, circle, shift=0):
+    radius = circle / 2.
+    step = 360 / number
+    points = [(radius * sin(radians(i * step + shift)),
+               radius * cos(radians(i * step + shift)))
+              for i in range(number)]
+    return points
+
+
+# Gears
+MODULE = 0.3
+Z_PINION = 18
+Z_GEAR = 60
+GEARS_MARGIN = 0.1
+
+# Motor
+MOTOR_SHIFT = 1.25
+MOTOR_DIAMETER = 17
+MOTOR_HOLE_DIAMETER = 6.3
+MOTOR_MOUNT_THICK = 2
+MOUNT_MINIHOLES_CIRCLE = 10
+MOUNT_MINIHOLES_DIAMETER = 1.7
+
+# Mount
+MOUNT_THICK = 5
+MOUNT_HEIGHT = MOTOR_DIAMETER
+MOUNT_WIDTH = 40
+MOUNT_FILLET = 1
+
+# Holes
+SCREW_SPACE = 34
+SCREW_DIAMETER = 2
+AXIS_DIAMETER = 2
+
+
+# Basic mount structure
+mount = cadquery.Workplane('XY').box(MOUNT_WIDTH, MOUNT_HEIGHT, MOUNT_THICK)
+
+# Base screws
+mount = mount.faces('<Y').workplane()\
+    .pushPoints([(-SCREW_SPACE / 2., 0), (SCREW_SPACE / 2., 0)])\
+    .hole(SCREW_DIAMETER)
+
+# Motor holes
+miniholes = circle_points(number=6, circle=MOUNT_MINIHOLES_CIRCLE)
+mount = mount.faces('<Z').workplane()\
+    .pushPoints(miniholes)\
+    .hole(diameter=MOUNT_MINIHOLES_DIAMETER)
+mount = mount.faces('>Z').workplane().cboreHole(
+    MOTOR_HOLE_DIAMETER,
+    MOTOR_DIAMETER + 1,
+    MOUNT_THICK - MOTOR_MOUNT_THICK)
+
+# Axis
+axis_shift = (MODULE * (Z_PINION + Z_GEAR)) / 2. + GEARS_MARGIN
+axis_shift *= cos(asin(MOTOR_SHIFT / axis_shift))
+mount = mount.faces('<Z').workplane()\
+    .pushPoints([(axis_shift, -MOTOR_SHIFT), (-axis_shift, -MOTOR_SHIFT)])\
+    .hole(AXIS_DIAMETER)
+
+# Fillet
+mount = mount.edges('|Z').fillet(MOUNT_FILLET)
+
+show(mount)
