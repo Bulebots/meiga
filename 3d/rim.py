70f4e9b22216ff3060ce5249c5d5b3f6b4b410f9
claraC <clara.casas.castedo@gmail.com>
2019-04-06 13:38:14 +0200

    fixup! Include 3d rim design

--- 3d/rim.py
+++ 3d/rim.py
@@ -1,30 +1,25 @@
 """
 This script defines the rim body.
 """
-from math import cos
-from math import sin
-from math import radians
-
 import cadquery
 from Helpers import show
 
 FDM_MARGIN = 0.1
-
-TIRE_W = 2.5 * 2
-WHEEL_D = 20.5
-WHEEL_D_FDM = WHEEL_D - FDM_MARGIN
-
-REEL_D0 = WHEEL_D_FDM - TIRE_W
 REEL_H0 = 8
+TIRE_W = 2.5
+WHEEL_D = 20.5
+PINION_D = 6.70
+
+OUTER_D = WHEEL_D - TIRE_W * 2 - FDM_MARGIN
 
 PINION_D = 6.70
-PINION_D_FDM = PINION_D + FDM_MARGIN
+INNER_D = PINION_D + FDM_MARGIN
 
 
 # Rim body
 rim = cadquery.Workplane('XY')\
-    .circle(radius=REEL_D0/2.).extrude(distance=REEL_H0)\
+    .circle(radius=OUTER_D/2.).extrude(distance=REEL_H0)\
     .faces('>Z').workplane()\
-    .hole(diameter=PINION_D_FDM)
+    .hole(diameter=INNER_D)
 
 show(rim)
