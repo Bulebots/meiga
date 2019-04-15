360433d6e4fbbe4a9e5365e06388a08fa9148318
Miguel Sánchez de León Peque <peque@neosit.es>
2019-04-16 00:50:05 +0200

    Add some CI to the project

---
+++ scripts/checkformat_checkpatch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+linux="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
+wget --no-clobber --quiet $linux/plain/scripts/spelling.txt
+wget --no-clobber --quiet $linux/plain/scripts/checkpatch.pl
+wget --no-clobber --quiet $linux/plain/scripts/const_structs.checkpatch
+chmod +x checkpatch.pl
+./checkpatch.pl --terse --no-tree --max-line-length=80 --ignore ARRAY_SIZE,BOOL_BITFIELD,LEADING_SPACE,SPDX_LICENSE_TAG,SPLIT_STRING,USE_NEGATIVE_ERRNO,VOLATILE --file src/*.[ch]
